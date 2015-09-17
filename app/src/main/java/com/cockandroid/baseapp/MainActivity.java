57e4ad53fa7d36dd34cc1c5c25994186f25e5911
skyidelete <skyidelete@naver.com>
2015-09-17 11:04:31 +0900

    2

--- app/src/main/java/com/cockandroid/baseapp/MainActivity.java
+++ app/src/main/java/com/cockandroid/baseapp/MainActivity.java
@@ -10,7 +10,8 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    Button button1;
+    Button button1, button2, button3, button4;
+
 
 
     @Override
@@ -18,11 +19,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
